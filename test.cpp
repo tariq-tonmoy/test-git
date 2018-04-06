490e840b24482e4f8959061ef05bdb464edfdd01
tariq-tonmoy <tariq_tonmoy@live.com>
2018-04-06 12:31:45 +0600

    Added function foo()

--- test.cpp
+++ test.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 
+int foo(int param) {
+	param = param + 12;
+	return param;
+}
+
 int main(){
 	int i = 1;
 
 	std::cout<<"Hello World"<<std::endl;
-	std::cout<<"The value is: "<<i+12<<std::endl;
+	std::cout<<"The value is: "<<foo()<<std::endl;
 
 	std::cout << "Good Bye" << std::endl;
 	return 0;
